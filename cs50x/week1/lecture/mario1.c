9d29ae466048ad53e55c310cb8d34ca3fd5284d3
abubakermubarak <abubakermubarak95@gmail.com>
2026-04-30 15:25:29 +0300

    printf bricks from supermario

---
+++ cs50x/week1/lecture/mario1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // For each row
+    for (int i = 0; i < 3; i++)
+    {
+        //For each column
+        for (int j = 0; j < 3; j++)
+        {
+            // print one brick
+            printf("#");
+        }
+    }
+}
