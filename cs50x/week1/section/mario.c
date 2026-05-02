758d582452fbfa27eb2849652289896a3110de90
abubakermubarak <abubakermubarak95@gmail.com>
2026-05-02 16:28:13 +0300

    intital verion

---
+++ cs50x/week1/section/mario.c
@@ -0,0 +1,13 @@
+#include "cs50.h"
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt user for height (int)
+    int height = get_int("height: ");
+    // Print pyramid of that height
+    for (int i = 0; i < height; i++)
+    {
+        printf("#");
+    }
+}
