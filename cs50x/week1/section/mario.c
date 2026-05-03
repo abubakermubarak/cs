26cb504fbc015acc4458ee832458a1dd3a73948e
abubakermubarak <abubakermubarak95@gmail.com>
2026-05-03 15:46:26 +0300

    make a function to printa row of bricks

--- cs50x/week1/section/mario.c
+++ cs50x/week1/section/mario.c
@@ -1,5 +1,7 @@
 #include "cs50.h"
 #include <stdio.h>
+//Prototypes
+void print_row(int bricks);
 
 int main(void)
 {
@@ -8,7 +10,15 @@ int main(void)
     // Print pyramid of that height
     for (int i = 0; i < height; i++)
     {
-        printf("#");
-        printf("\n");
+        print_row(i);
     }
+}
+//Given a number of bricks print that number of bricks
+void print_row(int bricks)
+{
+    for (int i = 0; i < bricks; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
 }
