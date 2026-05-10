9abdddd10ac6088c7305726c82bec026783cd145
abubakermubarak <abubakermubarak95@gmail.com>
2026-05-10 16:27:50 +0300

    generate random number with rand

--- cs50x/week1/pset/guess/guess.c
+++ cs50x/week1/pset/guess/guess.c
@@ -1,1 +1,15 @@
 #include "cs50.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    /*The program will give us the same result each time
+    * we need to seed the rand() with starting point
+    * we will used srand
+    */
+    //Generate random numbers
+    int random = rand();
+    printf("random number: %i\n", random);    
+}
