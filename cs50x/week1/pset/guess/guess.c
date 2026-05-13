17e2013d3c8b71d63dfda45f3f1e78cf32a69fe5
abubakermubarak <abubakermubarak95@gmail.com>
2026-05-13 15:38:48 +0300

    make a function to determine difficulty

--- cs50x/week1/pset/guess/guess.c
+++ cs50x/week1/pset/guess/guess.c
@@ -2,20 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+//Macros
+#define  ALLOWED_ATTEMPTS 7
+#define  DIFFICULTY 50
 //Prototypes
 int checkGuess(int guess, int number);
+int chooseDifficulty(void);
 int rand_int(int min, int max);
 
 int main(void)
 {
-    /*The program will give us the same result each time
-    * we need to seed the rand() with starting point
-    * we will used srand
-    */
     //Generate random numbers
     int random = rand_int(1, 100);
+    //Prompt user to choose difficulty
+    int choice = chooseDifficulty();
     //Prompt user to enter a guess
     int guess = get_int("Enter your guess: ");
+    int attempts = 1;
     int status = checkGuess(guess, random);
 
     while (status)
@@ -25,11 +28,13 @@ int main(void)
         {
             printf("Too high! Try again\n");
             guess = get_int("Enter your guess: ");
+            attempts++;
         }
         else if (guess < random)
         {
             printf("Too low! Try again\n");
             guess = get_int("Enter your guess: ");
+            attempts++;
         }
         else
         {
@@ -56,4 +61,14 @@ int checkGuess(int guess, int number)
         return -1;
     }
     return 0;
+}
+//Prompt user to chooses level of defcaulty
+int chooseDifficulty(void)
+{
+    int level;
+    do
+    {
+        level = get_int("Choose Difficulty:\n1. Easy   (1 - 50)\n2. Medium (1 - 100)\n3. Hard   (1 - 500)\n\nEnter choice: ");
+    } while (level < 1 || level > 3);
+    return level;
 }
